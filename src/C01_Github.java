8d71f413e03f34a0c1c96b4eebc6ef29de618a30
tugbatopcu <tugba.yldrm88@gmail.com>
2024-01-08 23:51:35 +0300

    first commit

---
+++ src/C01_Github.java
@@ -0,0 +1,11 @@
+public class C01_Github {
+
+    public static void main(String[] args) {
+
+        int a=128;
+        int b=258;
+        System.out.println("iki sayinin toplamı: " + (a+b));
+
+
+    }
+}
