ccf6f9af03c4b73e4a2b0defe835b359d1380aae
tugbatopcu <tugba.yldrm88@gmail.com>
2024-02-11 13:13:46 +0300

    11 subat

---
+++ src/P05.java
@@ -0,0 +1,9 @@
+public class P05 {
+    public static void main(String[] args) {
+
+        int sayı1= 5;
+        int sayi2=18;
+        System.out.println(sayı1*sayi2);
+
+    }
+}
--- src/P05Deneme.java
+++
@@ -1,9 +0,0 @@
-public class P05Deneme {
-
-    public static void main(String[] args) {
-
-        System.out.println("github deneme");
-
-
-    }
-}
